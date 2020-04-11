21be10e0584a3a29dd0d0c34e9c51d1c42c05898
xpaweee <krzysiek.kolsut@gmail.com>
2020-04-11 23:59:45 +0200

    Memory cards

---
+++ MemoryCards/script.js
@@ -0,0 +1,165 @@
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
+
+let currentActiveCard = 0;
+
+//Store cards
+
+const cardsEl = [];
+
+//Store card data
+
+const cardsData = getCardsData();
+
+// const cardsData = [
+//     {
+//         question:"Testowy pytanie",
+//         answer: "Odpowiedź"
+//     },
+//     {
+//         question:"Testowy pytanie2",
+//         answer: "Odpowiedź2"
+//     },
+//     {
+//         question:"Testowy pytanie3",
+//         answer: "Odpowiedź3"
+//     },
+//     {
+//         question:"Testowy pytanie4",
+//         answer: "Odpowiedź4"
+//     },
+// ];
+
+//Create all cards
+
+function createCards(){
+    cardsData.forEach((data,index) => createCard(data,index));
+}
+
+function createCard(data,index){
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if(index === 0){
+        card.classList.add('active');
+    }
+
+    card.innerHTML = `
+    <div class="inner-card">
+        <div class="inner-card-front">
+            <p>
+                ${data.question}
+            </p>
+        </div>
+        <div class="inner-card-back">
+            <p>
+                ${data.answer}
+            </p>
+        </div>
+    </div>
+    `;
+
+    card.addEventListener('click', () => {
+        card.classList.toggle('show-answer');
+    });
+
+    cardsEl.push(card);
+
+    cardsContainer.appendChild(card);
+
+    updateCurrentText();
+
+}
+
+function updateCurrentText(){
+    currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+function getCardsData(){
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return cards === null ? [] : cards;
+}
+
+
+function setCardsData(cards){
+    localStorage.setItem('cards', JSON.stringify(cards));
+    window.location.reload();
+}
+
+
+createCards();
+
+
+//Event listeners
+
+nextBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card left';
+
+    currentActiveCard = currentActiveCard +1;
+
+    if(currentActiveCard > cardsEl.length -1 ){
+        currentActiveCard = cardsEl.length -1;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentText();
+});
+
+
+prevBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card right';
+
+    currentActiveCard = currentActiveCard - 1 ;
+
+    if(currentActiveCard < 0 ){
+        currentActiveCard = 0;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentText();
+});
+
+
+showBtn.addEventListener('click', () => {
+    addContainer.classList.add('show');
+});
+
+hideBtn.addEventListener('click', () => {
+    addContainer.classList.remove('show');
+});
+
+
+addCardBtn.addEventListener('click', () => {
+    const question = questionEl.value;
+    const answer = answerEl.value;
+
+    if(question.trim() && answer.trim()){
+        const newCard = {question: question,answer: answer};
+        createCard(newCard);
+
+        questionEl.value = '';
+        answerEl.value = '';
+
+        addContainer.classList.remove('show');
+        cardsData.push(newCard);
+        setCardsData(cardsData);
+    }
+});
+
+clearBtn.addEventListener('click', () => {
+    localStorage.clear();
+    cardsContainer.innerHTML = '';
+    window.location.reload();
+})

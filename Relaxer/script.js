1404db2c7e117326271fc3a38961133351dcff9a
xpaweee <krzysiek.kolsut@gmail.com>
2020-04-12 18:43:24 +0200

    Cool relaxer

---
+++ Relaxer/script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = (totalTime / 5);
+
+function breathAnimation(){
+    text.innerText = 'Breathe In!';
+    container.classList = 'container grow';
+    setTimeout(() => {
+        text.innerText = 'Hold!';
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!';
+            container.classList = 'container shrink';
+
+        }, holdTime);
+    }, breatheTime)
+}
+
+setInterval(breathAnimation,totalTime);
+
+breathAnimation();
+
